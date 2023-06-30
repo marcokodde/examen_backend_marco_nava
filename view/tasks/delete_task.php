6cde8cd056363706fdeb7ae52ddc76c554ca5fac
Marko <70534584+marcokodde@users.noreply.github.com>
2023-06-30 08:53:50 -0600

    Deleting files

--- view/tasks/delete_task.php
+++
@@ -1,6 +0,0 @@
-<div class="row">
-    <div class="alert alert-success">
-        Tarea eliminada correctamente. <a href="index.php?controller=task&action=list">
-            Volver al listado</a>
-    </div>
-</div>
